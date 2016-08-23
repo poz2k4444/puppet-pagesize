53162a81208a1e23c97314301657d7bc1a6d04cf
poz2k4444 <poz2k4444@gmail.com>
2016-08-22 20:03:20 -0500

    Using recommended way for custom facts with simple resolutions

--- lib/facter/pagesize.rb
+++ lib/facter/pagesize.rb
@@ -1,11 +1,4 @@
 Facter.add('pagesize') do
   confine :kernel => 'Linux'
-  setcode do
-    binary = Facter::Core::Execution.exec('/usr/bin/which getconf')
-    if binary
-         Facter::Core::Execution.exec("#{binary} PAGESIZE")
-    else
-      return nil
-    end
-  end
+  setcode "getconf PAGESIZE"
 end
